1f6e13b3480b32740a38292cccc21fb56d660db1
linoy1 <linoysimantov@se.shenkar.ac.il>
2022-01-08 19:21:51 +0200

    update branch

--- user.py
+++ user.py
@@ -16,29 +16,4 @@ class User:
         self.email = email
         self.password = password
         self.role = role
-
-#####create table#####
-# mycursor.execute("CREATE TABLE User (id int auto_increment primary key, name varchar(255),phone int(255),"
-#                  "email varchar(255),password varchar(50),username varchar(50),type varchar(50))")
-
-###### insert user into system########
-# sql = "INSERT INTO User (id, name, phone, email, password, username, type) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-# val = ("123456", "Linoy Siman Tov", "0537404026", "lin.si.tov@gmail.com", "linoy123", "linoy1", "storekeeper")
-# mycursor.execute(sql, val)
-#
-# mydb.commit()
-
-# print(mycursor.rowcount, "record inserted.")
-
-#
-# mycursor.execute("DESCRIBE User")
-# for i in mycursor:
-#     print(i)
-
-#
-# mycursor.execute("SELECT * FROM User")
-#
-# myresult = mycursor.fetchall()
-#
-# for x in myresult:
-#   print(x)
+)
